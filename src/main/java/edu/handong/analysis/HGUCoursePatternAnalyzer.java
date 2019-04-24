dc66a1510cbebc12bc618fb627e16b2afc026ccc
SilverJun <jangej1031@naver.com>
2019-04-24 21:59:03 +0900

    Complete CoursePatternAnalyzer Class

--- src/main/java/edu/handong/analysis/HGUCoursePatternAnalyzer.java
+++ src/main/java/edu/handong/analysis/HGUCoursePatternAnalyzer.java
@@ -51,51 +51,77 @@ public class HGUCoursePatternAnalyzer {
 	/**
 	 * This method returns a Student array to initiate the field, students, from lines.
 	 * @param lines
-	 * @return
+	 * @return array of Student
 	 */
 	private Student[] initiateStudentArrayFromLines(String[] lines) {
 		
-		// TODO: implement this method
+		Student[] newStudents = new Student[numOfStudents];	// 배열 인스턴스
 		
+		int i = 0;
+		for (String line : lines)		// 라인마다 파싱하기 위한 for loop
+		{
+			String[] studentData = line.split(", ");
+			
+			Student newStudent = new Student(studentData[1]);
+			
+			if (!studentExist(newStudents, newStudent))		// 같은 이름을 찾지 못했을 경우, append.
+			{
+				newStudents[i] = newStudent;
+				i++;
+			}
+		}
 		
-		return null;
+		return newStudents;
 	}
 
 	/**
 	 * This method check if there is the same name of the second arugement in the array, students
 	 * @param students
 	 * @param student
-	 * @return boolean
+	 * @return boolean value. if student find, it returns true. or false.
 	 */
 	private boolean studentExist(Student[] students, Student student) {
-		
-		// TODO: implement this method
-
+		for (Student existStudent : students) // 현재 등록된 student중에 같은 이름이 있는지 탐색.
+			if (existStudent != null && student.getName().contentEquals(existStudent.getName()))
+				return true;
 		return false;
 	}
 	
 	/**
 	 * This method returns a Course array to initiate the field, courses, from lines.
 	 * @param lines
-	 * @return
+	 * @return array of Course
 	 */
 	private Course[] initiateCourseArrayFromLines(String[] lines) {
+		Course[] newCourses = new Course[numOfCourses];	// 배열 인스턴스
 		
-		// TODO: implement this method
+		int i = 0;
+		for (String line : lines)		// 라인마다 파싱하기 위한 for loop
+		{
+			String[] courseData = line.split(", ");
+			
+			Course newCourse = new Course(courseData[2]);
+			
+			if (!courseExist(newCourses, newCourse))		// 같은 이름을 찾지 못했을 경우, append.
+			{
+				newCourses[i] = newCourse;
+				i++;
+			}
+		}
 		
-		return null;
+		return newCourses;
 	}
 
 	/**
 	 * This method check if there is the same name of the second argument in the array, courses.
 	 * @param courses
 	 * @param course
-	 * @return boolean
+	 * @return boolean value. if student find, it returns true. or false.
 	 */
 	private boolean courseExist(Course[] courses, Course course) {
-		
-		// TODO: implement this method
-
+		for (Course existCourse : courses) // 현재 등록된 course중에 같은 course 이름이 있는지 탐색.
+			if (existCourse != null && course.getCourseName().contentEquals(existCourse.getCourseName()))
+				return true;
 		return false;
 	}
 
